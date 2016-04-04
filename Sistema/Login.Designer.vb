4e33653abbdfe8c09e54883410b66f6601b2314e
Cesar0513 <cesarivanorovil@gmail.com>
2016-04-04 16:49:58 -0500

    Login

--- Sistema/Login.Designer.vb
+++ Sistema/Login.Designer.vb
@@ -22,17 +22,77 @@ Partial Class Login
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Form1
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.txtPassword)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(551, 343)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(231, 202)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 22)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Contraseña"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPassword.Location = New System.Drawing.Point(211, 227)
+        Me.txtPassword.Multiline = True
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
+        Me.txtPassword.Size = New System.Drawing.Size(122, 32)
+        Me.txtPassword.TabIndex = 0
+        Me.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Panel2
+        '
+        Me.Panel2.BackgroundImage = Global.Sistema.My.Resources.Resources.portada
+        Me.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel2.Location = New System.Drawing.Point(65, 28)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(422, 120)
+        Me.Panel2.TabIndex = 2
+        '
+        'Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(538, 332)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(551, 343)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Panel1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Login"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "INICIO"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
 
 End Class
--- Sistema/Login.vb
+++ Sistema/Login.vb
@@ -1,3 +1,40 @@
-﻿Public Class Login
+﻿Imports MySql.Data
+Imports MySql.Data.MySqlClient
 
+Public Class Login
+
+#Region "VARIABLES GLOBALES"
+    Dim clsMysql As New Conexion
+#End Region
+
+#Region "SP"
+    Dim sp_ValidaUsuarioLogin As String = "Seguridad.sp_ValidaUsuarioLogin"
+#End Region
+
+#Region "SUB FUNCTION"
+
+    Public Sub ValidaUsuario(ByVal pwd As String)
+        Try
+            Dim dt As DataSet = clsMysql.EjecutaStore(sp_ValidaUsuarioLogin, pwd)
+        Catch ex As Exception
+                MsgBox("Error al Verificar Usuario", MsgBoxStyle.Information, "AVISO")
+        End Try
+
+    End Sub
+
+#End Region
+
+#Region "EVENTOS CONTROLADORES"
+
+    Private Sub txtPassword_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtPassword.KeyPress
+        If e.KeyChar = Microsoft.VisualBasic.ChrW(13) Then
+            If String.IsNullOrEmpty(txtPassword.Text) Then
+                MsgBox("Por favor ingrese su contraseña y presione ENTER", MsgBoxStyle.Exclamation, "AVISO")
+            Else
+                ValidaUsuario(txtPassword.Text)
+            End If
+        End If
+    End Sub
+
+#End Region
 End Class
