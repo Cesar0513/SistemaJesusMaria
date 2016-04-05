a23027284f2799a0609d406e08bde3346d0b2bf8
Cesar0513 <cesarivanorovil@gmail.com>
2016-04-05 17:17:21 -0500

    Resources

--- Sistema/My Project/Resources.Designer.vb
+++ Sistema/My Project/Resources.Designer.vb
@@ -93,6 +93,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property deduda() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("deduda", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property delete() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("delete", resourceCulture)
