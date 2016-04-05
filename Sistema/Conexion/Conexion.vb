2cad1a58c2cf2041bd05f7e069ce77b341b68d37
Cesar0513 <cesarivanorovil@gmail.com>
2016-04-05 09:05:43 -0500

    Agregar

---
+++ Sistema/Conexion/Conexion.vb
@@ -0,0 +1,103 @@
+﻿Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Public Class Conexion
+
+    Private Comm As New MySqlCommand
+    Dim conString As String = "server=Miservidor; user id= myusuario ; password=myContraseña"
+    Public con As New MySqlConnection(conString)
+
+    Public Sub AbrirConexion()
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+
+        Try
+            con.Open()
+        Catch ex As Exception
+            MsgBox("Error al abrir la conexión")
+        End Try
+    End Sub
+
+
+    Public Function EjecutaStore(ByVal NombreStore As String, ByVal ParamArray parametros() As Object) As DataTable
+        Dim dt As New DataTable
+        Try
+            Dim da As New MySqlDataAdapter
+            Dim comm As New MySqlCommand(NombreStore, con)
+            comm.CommandType = CommandType.StoredProcedure
+            AbrirConexion()
+
+            'carga parametros del store procedure
+            MySqlCommandBuilder.DeriveParameters(comm)
+            Dim i As Integer = 0
+            For Each parameter As MySqlParameter In comm.Parameters
+                If parameter.Direction = ParameterDirection.Input Or parameter.Direction = ParameterDirection.InputOutput Then
+                    parameter.Value = parametros(i)
+                    i = i + 1
+                    'para obtener el nombre del parametro  
+                    'parameter.ParameterName
+                End If
+            Next
+
+            'comm.Parameters.AddWithValue("@" & nombre & "", nombre)
+            'comm.Parameters.AddWithValue("@" & valor & "", valor)
+
+            da.SelectCommand = comm
+            da.SelectCommand.Connection = con
+
+            Dim ds As MySqlDataAdapter = New MySqlDataAdapter(comm)
+            da.Fill(dt)
+            con.Close()
+            Return dt
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+            Dim ds As DataSet = Nothing
+            Return dt
+        End Try
+    End Function
+
+
+    Public Function ejecutaProcedimiento(ByVal strSPNombre As String, ByVal ParamArray arrSPParametros() As Object) As DataTable
+        Dim objConexion As MySqlConnection
+        Dim objComando As MySqlCommand
+
+        Dim dt As New DataTable
+        Try
+
+
+            objConexion = New MySqlConnection(conString)
+            If objConexion.State = Data.ConnectionState.Open Then objConexion.Close()
+            objConexion.Open()
+            objComando = New MySqlCommand(strSPNombre, objConexion)
+            objComando.CommandType = CommandType.StoredProcedure
+            objComando.CommandTimeout = 0
+
+            'carga parametros del store procedure
+            MySqlCommandBuilder.DeriveParameters(objComando)
+            Dim i As Integer = 0
+            For Each parameter As MySqlParameter In objComando.Parameters
+                If parameter.Direction = ParameterDirection.Input Or parameter.Direction = ParameterDirection.InputOutput Then
+                    parameter.Value = arrSPParametros(i)
+                    i = i + 1
+                    'para obtener el nombre del parametro  
+                    'parameter.ParameterName
+                End If
+            Next
+
+            Dim da As MySqlDataAdapter = New MySqlDataAdapter(objComando)
+            da.Fill(dt)
+
+        Catch ex As Exception
+            If objConexion.State = Data.ConnectionState.Open Then objConexion.Close()
+            Throw (ex)
+        Finally
+            dt.Dispose()
+            objComando.Dispose()
+            objConexion.Close()
+            objConexion.Dispose()
+        End Try
+        Return dt
+
+    End Function
+
+End Class
---
+++ Sistema/Principal.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Principal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabAdmin = New System.Windows.Forms.TabPage()
+        Me.TabUsuarios = New System.Windows.Forms.TabPage()
+        Me.TabRedes = New System.Windows.Forms.TabPage()
+        Me.TabServ = New System.Windows.Forms.TabPage()
+        Me.TabReconexion = New System.Windows.Forms.TabPage()
+        Me.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.SplitContainer1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1220, 670)
+        Me.Panel1.TabIndex = 0
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Panel2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.DateTimePicker1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.TabControl1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(1220, 670)
+        Me.SplitContainer1.SplitterDistance = 152
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Location = New System.Drawing.Point(13, 12)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(281, 124)
+        Me.Panel2.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(83, 70)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(125, 22)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Administrador "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(83, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(109, 22)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Cesar Orozco"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(32, 74)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Perfil:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(16, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Usuario:"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker1.Location = New System.Drawing.Point(975, 12)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 25)
+        Me.DateTimePicker1.TabIndex = 0
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabAdmin)
+        Me.TabControl1.Controls.Add(Me.TabUsuarios)
+        Me.TabControl1.Controls.Add(Me.TabRedes)
+        Me.TabControl1.Controls.Add(Me.TabServ)
+        Me.TabControl1.Controls.Add(Me.TabReconexion)
+        Me.TabControl1.Font = New System.Drawing.Font("Palatino Linotype", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TabControl1.Location = New System.Drawing.Point(5, 3)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1212, 508)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabAdmin
+        '
+        Me.TabAdmin.Location = New System.Drawing.Point(4, 26)
+        Me.TabAdmin.Name = "TabAdmin"
+        Me.TabAdmin.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabAdmin.Size = New System.Drawing.Size(1204, 478)
+        Me.TabAdmin.TabIndex = 0
+        Me.TabAdmin.Text = "ADMINISTRADOR"
+        Me.TabAdmin.UseVisualStyleBackColor = True
+        '
+        'TabUsuarios
+        '
+        Me.TabUsuarios.BackColor = System.Drawing.Color.Silver
+        Me.TabUsuarios.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TabUsuarios.Location = New System.Drawing.Point(4, 26)
+        Me.TabUsuarios.Name = "TabUsuarios"
+        Me.TabUsuarios.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabUsuarios.Size = New System.Drawing.Size(1204, 478)
+        Me.TabUsuarios.TabIndex = 1
+        Me.TabUsuarios.Text = "USUARIOS"
+        '
+        'TabRedes
+        '
+        Me.TabRedes.Location = New System.Drawing.Point(4, 26)
+        Me.TabRedes.Name = "TabRedes"
+        Me.TabRedes.Size = New System.Drawing.Size(1204, 478)
+        Me.TabRedes.TabIndex = 2
+        Me.TabRedes.Text = "REDES"
+        Me.TabRedes.UseVisualStyleBackColor = True
+        '
+        'TabServ
+        '
+        Me.TabServ.Location = New System.Drawing.Point(4, 26)
+        Me.TabServ.Name = "TabServ"
+        Me.TabServ.Size = New System.Drawing.Size(1204, 478)
+        Me.TabServ.TabIndex = 3
+        Me.TabServ.Text = "TIPO SERVICIO"
+        Me.TabServ.UseVisualStyleBackColor = True
+        '
+        'TabReconexion
+        '
+        Me.TabReconexion.Location = New System.Drawing.Point(4, 26)
+        Me.TabReconexion.Name = "TabReconexion"
+        Me.TabReconexion.Size = New System.Drawing.Size(1204, 478)
+        Me.TabReconexion.TabIndex = 4
+        Me.TabReconexion.Text = "RECONEXION"
+        Me.TabReconexion.UseVisualStyleBackColor = True
+        '
+        'Principal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1220, 670)
+        Me.Controls.Add(Me.Panel1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Principal"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.TopMost = True
+        Me.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabAdmin As System.Windows.Forms.TabPage
+    Friend WithEvents TabUsuarios As System.Windows.Forms.TabPage
+    Friend WithEvents TabRedes As System.Windows.Forms.TabPage
+    Friend WithEvents TabServ As System.Windows.Forms.TabPage
+    Friend WithEvents TabReconexion As System.Windows.Forms.TabPage
+End Class
